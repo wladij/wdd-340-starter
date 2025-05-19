32e71994edc4b2f634ace535a1e0a4eaac4ff466
wladij <joew.candelario@gmail.com>
2025-05-18 20:57:17 -0500

    update file sql

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO demo345;
+    OWNER TO demo340;
 
 -- Table structure for table 'classification'
 CREATE TABLE public.classification(
